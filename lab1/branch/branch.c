d72f12f563c9df4eef204e2e8f294ac065db5eb7
rvgorlov <rv.gorlov@gmail.com>
2017-03-19 11:24:08 +0700

    fix lab1

--- lab1/branch/branch.c
+++ lab1/branch/branch.c
@@ -10,7 +10,7 @@
 
 enum { 
     n = 100000,
-    nreps = 20 
+    nreps = 100 
 };
 
 double x[n], y[n], z[n];
@@ -32,14 +32,36 @@ void blend_map(double *dest, double *a, double *b, int size, int blend)
 
 void blend_map_opt(double *dest, double *a, double *b, int size, int blend)
 {
-    /* TODO: Implement optimized version of blend_map */
+    int i = 0; 
+
+    if (blend == 255)
+    {
+        for (i = 0; i < size; ++i)
+        {
+            dest[i] = a[i];
+        }
+    }
+    else if (blend == 0)
+    {
+        for (i = 0; i < size; ++i)
+        {
+            dest[i] = b[i];
+        }    
+    }
+    else
+    {
+        for (i = 0; i < size; ++i)
+        {
+            dest[i] = a[i] * blend + b[i] * (255 - blend) / 256.0;
+        }
+    }
 }
 
 int main()
 {
     double tfirst, t;
     int i;
-        
+    
     /* First run: warmup */
     tfirst = hpctimer_wtime();
     blend_map(z, x, y, n, 0);
